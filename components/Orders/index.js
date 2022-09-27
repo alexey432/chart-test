7104c6457322bc28752730dc2959238983d36799
alexey432 <alexorl432@gmail.com>
2022-09-26 21:25:04 -0700

    intervals are added

--- components/Orders/index.js
+++ components/Orders/index.js
@@ -3,12 +3,12 @@ import { StyleSheet, Text, View } from 'react-native'
 
 const index = ({ asks, bids }) => {
 
-    console.log('Asks: ', asks);
-    console.log('Bids: ', bids);
+    // console.log('Asks: ', asks);
+    // console.log('Bids: ', bids);
     
     const showAsks = () => {
-        for(let i = 0; i < asks.length; i++) {
-            return <View key={i}>
+        for(let i = 0; i < asks.length; i += 2) {
+            return <View key={i} style={styles.rowContainer}>
                 <Text style={styles.textColor}>{asks[i]}</Text>
                 <Text style={styles.textColor}>{asks[i+1]}</Text>
             </View>
@@ -21,20 +21,24 @@ const index = ({ asks, bids }) => {
             <View style={{ flex: 1, marginRight: 20 }}>
                 <Text style={styles.textColor}>Bid</Text>
                 {bids.map((item, index, array) => {
-                    return <View key={index} style={styles.rowContainer}>
-                    <Text style={styles.textColor}>{array[index]}</Text>
-                    <Text style={styles.textColor}>{array[index+1]}</Text>
-                </View>
+                    if(index % 2 === 0){
+                        return <View key={index} style={{...styles.rowContainer, ...styles.bidRow}}>
+                                <Text style={styles.textColor}>{array[index]}</Text>
+                                <Text style={styles.textColor}>{array[index+1]}</Text>
+                            </View>
+                    }
                 })}
             </View>
 
             <View style={{ flex: 1 }}> 
                 <Text style={styles.textColor}>Ask</Text>
                 {asks.map((item, index, array) => {
-                    return <View key={index} style={styles.rowContainer}>
-                    <Text style={styles.textColor}>{array[index]}</Text>
-                    <Text style={styles.textColor}>{array[index+1]}</Text>
-                </View>
+                    if(index % 2 === 0){
+                        return <View key={index} style={{...styles.rowContainer, ...styles.askRow}}>
+                                <Text style={styles.textColor}>{array[index]}</Text>
+                                <Text style={styles.textColor}>{array[index+1]}</Text>
+                            </View>
+                    }
                 })}
             </View>
 
@@ -52,6 +56,14 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         // flex: 1
     },
+    bidRow: {
+        backgroundColor: 'rgba(0,255,0,0.2)',
+        marginBottom: 2
+    },
+    askRow: {
+        backgroundColor: 'rgba(255,0,0,0.2)',
+        marginBottom: 2
+    },  
     container: {
         marginTop: 30,
         borderTop: 10,
--- components/screens/CryptoDetail.js
+++ components/screens/CryptoDetail.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, StyleSheet, Button } from "react-native";
+import { View, StyleSheet, Button, SafeAreaView, ScrollView } from "react-native";
 import Chart from "../Chart/Chart";
 import data from "../Chart/data.json";
 import axios from "axios";
@@ -17,103 +17,105 @@ const CryptoDetail = () => {
     asks: [],
     bids: []
   });
-  const [interval, setInterval] = useState(1);
+  const [timeframe, setTimeframe] = useState(1);
 
   useEffect(() => {
-    async function getData() {
-      try {
-        const response = await axios.get(
-          `https://api.poloniex.com/markets/ETH_USDT/candles?interval=MINUTE_${interval}`
-        );
-        //   console.log(response.data);
-  
-        const newArr = [];
-  
-        response.data.map((item) => {
-          const newObj = {
-            low: item[0],
-            high: item[1],
-            open: item[2],
-            close: item[3],
-          };
-          newArr.push(newObj);
-        });
-  
-        setCandles(newArr);
+    getChartData();
+    const intervalId = setInterval(getChartData, 5000);
 
-      } catch (error) {
-        console.log(error);
-        
-      }
-    }
+    return () => clearInterval(intervalId);
 
-    getData();
-    
-    // const intervalId = setInterval(() => {
-
-    // }, 10000)
-
-    // return () => clearInterval(intervalId)
-
-  }, [interval]);
+  }, [timeframe]);
 
   useEffect(() => {
-    async function getData() {
-      try {
-        const response = await axios.get(
-          `https://api.poloniex.com/markets/ETH_USDT/orderBook?scale=1&limit=10`
-        );
-        //   console.log(response.data);
-        // console.log(JSON.stringify(response.data, null, 2));
-          const newObj = {
-            asks: response.data.asks,
-            bids: response.data.bids
-          }
+    const intervalId = setInterval(getOrdersData, 5000);
 
-          setOrders({...newObj})
-
-      } catch (error) {
-        console.log(error);
-        
-      }
-    }
-
-    getData();
+    return () => clearInterval(intervalId);
   }, []);
-  // console.log(orders);
   
+  
+  async function getOrdersData() {
+    try {
+      const response = await axios.get(
+        `https://api.poloniex.com/markets/ETH_USDT/orderBook?scale=1&limit=10`
+      );
+      //   console.log(response.data);
+      // console.log(JSON.stringify(response.data, null, 2));
+        const newObj = {
+          asks: response.data.asks,
+          bids: response.data.bids
+        }
 
-  // console.log(candlesNew);
+        setOrders({...newObj})
+        console.log('orders updated');
+        
+
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
+  
+  async function getChartData() {
+    try {
+      const response = await axios.get(
+        `https://api.poloniex.com/markets/ETH_USDT/candles?interval=MINUTE_${timeframe}&limit=50`
+      );
+      //   console.log(response.data);
+
+      const newArr = [];
+
+      response.data.map((item) => {
+        const newObj = {
+          low: item[0],
+          high: item[1],
+          open: item[2],
+          close: item[3],
+        };
+        newArr.push(newObj);
+      });
+
+      setCandles(newArr);
+      console.log('chart updated');
+
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
 
   const candles = candlesNew.slice(0, 50);
   const domain = getDomain(candles);
   // console.log(domain);
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <View style={styles.btnsContainer}>
         <Button
-          onPress={() => setInterval(1)}
+          onPress={() => setTimeframe(1)}
           title="1m"
           color="#841584"
           accessibilityLabel="1m timeframe"
         />
         <Button
-          onPress={() => setInterval(5)}
+          onPress={() => setTimeframe(5)}
           title="5m"
           color="#841584"
           accessibilityLabel="5m timeframe"
         />
         <Button
-          onPress={() => setInterval(10)}
+          onPress={() => setTimeframe(10)}
           title="10m"
           color="#841584"
           accessibilityLabel="10m timeframe"
         />
       </View>
       {candles.length > 0 ? <Chart {...{ candles, domain }} /> : null}
+
+      <ScrollView>
       {orders.asks.length > 0 && orders.bids.length > 0 ? <Orders asks={orders.asks} bids={orders.bids} /> : null}
-    </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
